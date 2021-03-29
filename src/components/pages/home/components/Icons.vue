529a164c351d45a3c953a4c3e49bfe180643e16e
lucas_dong <lucas_dong@xin-yin.net>
2021-03-29 10:09:50 +0800

    圖標區域邏輯

--- src/components/pages/home/components/Icons.vue
+++ src/components/pages/home/components/Icons.vue
@@ -1,53 +1,15 @@
 <template>
   <div class="icons">
-     <div class="icon">
-       <div class="icon-img">
-        <img class="icon-img-content" src='https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png'>
-       </div>
-       <p class="icon-desc">海外酒店</p>
-      </div>
-      <div class="icon">
-       <div class="icon-img">
-        <img class="icon-img-content" src='https://picbed.qunarzz.com/a36d2288f19e54562338f4d8ef986288.png'>
-       </div>
-       <p class="icon-desc">低價機票</p>
-      </div>
-      <div class="icon">
-       <div class="icon-img">
-        <img class="icon-img-content" src='https://picbed.qunarzz.com/377db8cb2143aebf01869c9baad3d325.png'>
-       </div>
-       <p class="icon-desc">汽車票船票</p>
-      </div>
-      <div class="icon">
-       <div class="icon-img">
-        <img class="icon-img-content" src='https://picbed.qunarzz.com/ae617a31e0bd5803d76918b817f6d942.png'>
-       </div>
-       <p class="icon-desc">自由行</p>
-      </div>
-      <div class="icon">
-       <div class="icon-img">
-        <img class="icon-img-content" src='https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png'>
-       </div>
-       <p class="icon-desc">海外酒店</p>
-      </div>
-      <div class="icon">
-       <div class="icon-img">
-        <img class="icon-img-content" src='https://picbed.qunarzz.com/a36d2288f19e54562338f4d8ef986288.png'>
-       </div>
-       <p class="icon-desc">低價機票</p>
-      </div>
-      <div class="icon">
-       <div class="icon-img">
-        <img class="icon-img-content" src='https://picbed.qunarzz.com/377db8cb2143aebf01869c9baad3d325.png'>
-       </div>
-       <p class="icon-desc">汽車票船票</p>
-      </div>
-      <div class="icon">
-       <div class="icon-img">
-        <img class="icon-img-content" src='https://picbed.qunarzz.com/ae617a31e0bd5803d76918b817f6d942.png'>
-       </div>
-       <p class="icon-desc">自由行</p>
-      </div>
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src='item.imgUrl'>
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
@@ -56,6 +18,57 @@ export default {
   name: 'HomeIcons',
   data () {
     return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png',
+        desc: '海外酒店海外酒店海外酒店海外酒店海外酒店'
+      }, {
+        id: '0002',
+        imgUrl: 'https://picbed.qunarzz.com/a36d2288f19e54562338f4d8ef986288.png',
+        desc: '低價機票'
+      }, {
+        id: '0003',
+        imgUrl: 'https://picbed.qunarzz.com/377db8cb2143aebf01869c9baad3d325.png',
+        desc: '汽車票船票'
+      }, {
+        id: '0004',
+        imgUrl: 'https://picbed.qunarzz.com/ae617a31e0bd5803d76918b817f6d942.png',
+        desc: '自由行'
+      }, {
+        id: '0005',
+        imgUrl: 'https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png',
+        desc: '海外酒店'
+      }, {
+        id: '0006',
+        imgUrl: 'https://picbed.qunarzz.com/a36d2288f19e54562338f4d8ef986288.png',
+        desc: '低價機票'
+      }, {
+        id: '0007',
+        imgUrl: 'https://picbed.qunarzz.com/377db8cb2143aebf01869c9baad3d325.png',
+        desc: '汽車票船票'
+      }, {
+        id: '0008',
+        imgUrl: 'https://picbed.qunarzz.com/ae617a31e0bd5803d76918b817f6d942.png',
+        desc: '自由行'
+      }, {
+        id: '0009',
+        imgUrl: 'https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png',
+        desc: '海外酒店'
+      }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
     }
   }
 }
@@ -63,10 +76,9 @@ export default {
 
 <style lang='stylus' socped>
   @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
 
-  .icons
-    width: 100%
-    overflow:hidden
+  .icons,.swiper-container
     height: 0
     padding-bottom: 50%
     .icon
@@ -97,4 +109,5 @@ export default {
         line-height : .44rem
         color: $darkTextColor
         text-align: center
+        ellipsit()
 </style>
