80411ebe7a8d79d10b0b68ebe3e2cd631c9606a9
lucas_dong <lucas_dong@xin-yin.net>
2021-04-01 10:20:21 +0800

    詳細頁面列表組件遞回

--- src/components/pages/detail/Detail.vue
+++ src/components/pages/detail/Detail.vue
@@ -2,6 +2,7 @@
   <div>
     <DetailBanner></DetailBanner>
     <DetailHeader></DetailHeader>
+    <DetailList :list="list"></DetailList>
     <div class="content"></div>
   </div>
 </template>
@@ -9,12 +10,43 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人個人票',
+              children: [
+                {
+                  title: '一日票'
+                }, {
+                  title: '三日票'
+                }
+              ]
+            }, {
+              title: '成人團體票'
+            }
+          ]
+        }, {
+          title: '學生票'
+        }, {
+          title: '兒童票'
+        }, {
+          title: '優惠票'
+        }
+      ]
+    }
   }
 }
 </script>
